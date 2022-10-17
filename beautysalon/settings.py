e91ce43c03e32a38339bd5caf2bcc0f6ed11f437
Ilya <ilya.shelepov2000@gmail.com>
2022-10-17 21:04:42 +0300

    Add models: User, Service, Visit

--- beautysalon/settings.py
+++ beautysalon/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'main.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   BASE_DIR / "static",
    ]
 
 # Default primary key field type
