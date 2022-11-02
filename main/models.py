74b21bb6eefbafeeecefd37c002339ea236fa4e9
Ilya <ilya.shelepov2000@gmail.com>
2022-11-02 20:28:33 +0300

    Add property end_date_time

--- main/models.py
+++ main/models.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 
-
 class Service(models.Model):
     name = models.CharField(_("Service name"), max_length=255)
-    duration = models.DurationField(_('Duration in minutes'))
+    duration = models.DurationField(_('Duration in seconds'))
     
     def __str__(self):
         return self.name
+
 
 class Visit(models.Model):
     date_time = models.DateTimeField(_("Date and time of visit"))
@@ -17,5 +17,9 @@ class Visit(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    @property
+    def end_date_time(self):
+        return self.date_time + self.service.duration
+    
     def __str__(self):
         return str(self.date_time)
